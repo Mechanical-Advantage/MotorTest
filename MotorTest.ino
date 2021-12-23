becd430ed1c1af052a17ac501fefa4eccdc3a817
V-RA <23vinamra@gmail.com>
2021-12-23 17:59:33 -0500

    added propotional output and toggle debounce button on or off
    Co-authored-by: AryanKulkarni <AryanKulkarni@users.noreply.github.com>

--- MotorTest.ino
+++ MotorTest.ino
@@ -1,79 +1,113 @@
 #include <Servo.h>
-#include <Wire.h> 
-Servo myservo;   
+#include <Wire.h>
+Servo myservo;
 
 //add channel state (simple bool)
 
-const int buttonPin = 1; 
-const int ledPin = 2;  
-const double deadBand = 50;  
+const int buttonPin = 1;
+const int ledPin = 2;
+const double deadBand = 0.5; 
+const double gain = 200;
 
-bool channel1; 
-bool channel2; 
-bool channel3; 
+bool channel1;
+bool channel2;
+bool channel3;
 bool channel4;
 
-int pwmValue; 
-int sliderValue;
-int buttonState; 
-int ledState = LOW; 
-int lastButtonState = LOW;  
+int pwm1Value;
+int pwm2Value;
+int pwm3Value;
+int pwm4Value;
+int slider1Value;
+int slider2Value;
+int slider3Value;
+int slider4Value;
+int buttonState;
+int ledState = LOW;
+int lastButtonState = LOW;
 int buttonReading;
- 
-unsigned long debounceTime = 0; 
+
+unsigned long debounceTime = 0;
 unsigned long debounceDelay = 50;
 
-void setup(){ 
-  channel1 = false; 
-  channel2 = false; 
-  channel3 = false; 
+void setup()
+{
+  channel1 = false;
+  channel2 = false;
+  channel3 = false;
   channel4 = false;
 
-  pinMode(buttonPin, INPUT_PULLUP);   
-  pinMode(ledPin, OUTPUT); 
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
   myservo.attach(3);
 }
 
-void loop(){  
-  buttonReading = digitalRead(buttonPin);  
-  pwmValue = analogRead(D3); //PWM input first pin: D3
-  sliderValue = analogRead(A4); //analog input first pin: A4 
+void loop()
+{
+  //pwmValue = digitalRead(D3);    //PWM input first pin: D3
+  slider1Value = analogRead(A4); //analog input first pin: A4
+  slider2Value = analogRead(A5);
+  slider3Value = analogRead(A6);
+  slider4Value = analogRead(A7);
+
+  int pwm1Value = map(slider1Value, 0, 1023, 0, 5);
+
+
+
+  //convert analog reading from a scale of 0-1023 to a scale of 0-5(volts)  
   
-  int pwnValue = map(sliderValue, 0, 1023, 1000, 2000);
- //convert analog reading from a scale of 0-1023 to a scale of 0-5(volts) 
-
-//set motor speeds based on analog inputs
-  if(pwmValue < (1500 + deadBand) && pwnValue > (1500 - deadBand)) { 
-    myservo.writeMicroseconds(1500);
+  if (pwm1Value < (2.5 + deadBand) && pwm1Value > (2.5 - deadBand))
+  {
+    myservo.writeMicroseconds(1500);// stop
   }
 
-  if(pwmValue > (2.5 + deadBand)) { 
-    myservo.writeMicroseconds(2000);
-  }  
-
-  if(pwmValue < (2.5 - deadBand)) { 
-    myservo.writeMicroseconds(1000);
+  if (pwm1Value > (2.5 + deadBand))
+  {
+    myservo.writeMicroseconds(((pwm1Value - (2.5 + deadBand)) * gain) + 1500);
   }
 
-//Debounce Button code --> turn this into a function
-}  
+  if (pwm1Value < (2.5 - deadBand))
+  {
+    myservo.writeMicroseconds(((pwm1Value - (2.5 + deadBand)) * gain) + 1500);
+  }
 
+ 
+}
+  
 
-int isButtonPressed(int ledPin, int buttonPin, int lastButtonState, int lastLedState) {
+ //Debounce Button code 
+void isButtonPressed(int ledPin, int buttonPin, int lastButtonState, int lastLedState, bool channel)
+{
+  buttonReading = digitalRead(buttonPin); 
 
-  if(buttonReading != lastButtonState) {
-    debounceTime = millis(); 
-  } 
-  if(millis() - debounceTime > debounceDelay){ 
-    if(buttonReading != buttonState) {
+  if (buttonReading != lastButtonState)
+  {
+
+    debounceTime = millis();
+
+    if (millis() - debounceTime > debounceDelay)
+    { 
       buttonState = buttonReading;
-    } 
-    if(buttonState == HIGH){ 
+
+    if (buttonState == HIGH)
+    {
       ledState = HIGH;
     }
-  } 
+    else if (buttonState == LOW)
+    {
+      ledState = LOW;
+    }
+    
+    }
+  }
+  
 
-  digitalWrite(ledPin, ledState);  
+  digitalWrite(ledPin, ledState);
 
-  lastButtonState = buttonReading; 
-} 
+  lastButtonState = buttonReading;
+
+}
+
+
+
+ 
